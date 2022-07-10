b65501e20fefb23a43abb3c18c5583103ae06041
vivek belwal <vivekbelwal144@gmail.com>
2022-07-10 16:02:27 +0530

    minor changes

--- routes/posts.js
+++ routes/posts.js
@@ -51,7 +51,7 @@ router.post("/", async (req, res) => {
   try {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
-  } catch {
+  } catch(err) {
     res.status(500).json(err);
   }
 });
