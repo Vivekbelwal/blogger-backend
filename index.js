efbdfff539be25ddc724b20886ab5d7922df0229
Vivek Belwal <vivekbelwal144@gmail.com>
2025-06-27 18:21:02 +0530

    polishing

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
-const cors = require('cors');
+const cors = require("cors");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 dotenv.config();
 app.use(express.json());
@@ -47,6 +52,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen(process.env.PORT||5000, () => {
-  console.log("Backend is running at port...");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend is running at port 5000...");
 });
